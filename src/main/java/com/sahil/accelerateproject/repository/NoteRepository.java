990d18894c8c69536ed5f6e3bb37e0a8800479ac
innovativesahil <innovativesahil95@gmail.com>
2019-04-22 21:13:25 +0530

    Created an interface to perform CRUD Operations for the Note

---
+++ src/main/java/com/sahil/accelerateproject/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package com.sahil.accelerateproject.repository;
+
+import com.sahil.accelerateproject.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
