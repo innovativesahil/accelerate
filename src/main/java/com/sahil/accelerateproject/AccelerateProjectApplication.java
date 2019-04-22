4609d8412260ab6105f4211336816c6454ed3920
innovativesahil <innovativesahil95@gmail.com>
2019-04-22 20:20:34 +0530

    Created initial project structure and added the dependencies

---
+++ src/main/java/com/sahil/accelerateproject/AccelerateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sahil.accelerateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccelerateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccelerateProjectApplication.class, args);
+    }
+
+}
